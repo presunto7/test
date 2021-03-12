9f0ceab185ac846c722d2c8da2a5e7c5eb3f7aac
ninolomata <Bruno.Vilaca.Sa@gmail.com>
2021-03-12 11:02:09 +0000

    hh

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
- import subprocess
- subprocess.call('cd .. ; doxygen', shell=True)
+import subprocess
+subprocess.call('cd .. ; doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
