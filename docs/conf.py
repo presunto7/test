f6bca1f8772b1d0b361f219cd1b15b75572102b9
Bruno Sá <bruno.vilaca.sa@gmail.com>
2021-04-08 17:56:28 +0100

    h

--- docs/conf.py
+++ docs/conf.py
@@ -38,10 +38,9 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "breathe"
-]
+extensions = [ "breathe" ]
 # Breathe Configuration
-breathe_default_project = "test"
+breathe_default_project = "y"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
